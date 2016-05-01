79aa69422c03dc42b68f63af4bc2672fa9460ca8
Matthew Dennis <dennmat@gmail.com>
2016-05-01 15:58:49 -0400

    Starting to build out the screenmanager

--- proj.win32/Screen.cpp
+++ proj.win32/Screen.cpp
@@ -6,29 +6,33 @@ ScreenManager::ScreenManager() {
 
 }
 
-short ScreenManager::stackScreen(const Screen* screen) {
+short ScreenManager::stackScreen(std::unique_ptr<Screen> screen) {
+	this->screens.push_back(screen);
+}
+
+std::unique_ptr<Screen> ScreenManager::popScreen() {
+	std::unique_ptr<Screen> screen = std::move(this->screens.at(this->screens.size() - 1));
+	
+	this->unsetupScreen(screen);
+
+	this->screens.pop_back();
+
+	return screen;
+}
+
+void ScreenManager::focusScreen(Screen* screen) {
 
 }
 
-const gnh::Screen& ScreenManager::popScreen() {
+void ScreenManager::focusScreenAt(const short index) {
 
 }
 
-void ScreenManager::focusScreen(Screen& screen) {
+void ScreenManager::showScreen(Screen* screen) {
 
 }
 
-void ScreenManager::focusScreenAt(const short index)
-{
-
-}
-
-void ScreenManager::showScreen(Screen& screen)
-{
-
-}
-
-void ScreenManager::hideScreen(Screen& screen) {
+void ScreenManager::hideScreen(Screen* screen) {
 
 }
 
@@ -36,8 +40,8 @@ void ScreenManager::removeScreenAt(const short index) {
 
 }
 
-void ScreenManager::loadScreen(Screen& screen) {
-
+void ScreenManager::loadScreen(Screen* screen) {
+	this->setupScreen(screen);
 }
 
 void ScreenManager::handleEvent(cocos2d::Event* event, cocos2d::Touch* touch/*=nullptr*/) {
@@ -48,15 +52,80 @@ bool ScreenManager::propagateEvent(cocos2d::Event* event, cocos2d::Touch* touch/
 
 }
 
-ScreenManager::~ScreenManager() {
+void ScreenManager::setupScreen(Screen* screen) {
+	screen->setupListeners();
 
+	if (screen->is_modal) {
+		//Prevent all other listeners, save current listeners state
+	}
 }
 
-DialogScreen::DialogScreen() {
+void ScreenManager::unsetupScreen(const std::unique_ptr<Screen>& screen) {
+	if (screen->is_modal) {
+		//Return listeners to state before screen was setup
+	}
 
+	screen->unload();
+}
+
+void ScreenManager::update() {
+	for (const auto& screen : this->screens) {
+		screen->update();
+	}
+}
+
+void ScreenManager::render() {
+	for (const auto& screen : this->screens) {
+		if (screen->is_visible) {
+			screen->render();
+		}
+	}
+}
+
+ScreenManager::~ScreenManager() {}
+
+DialogScreen::DialogScreen() {
+	this->has_touch = true;
+	this->has_mouse = true;
+	this->has_keyboard = false;
 }
 
 short DialogScreen::load() {
+	this->mouse_listener = cocos2d::EventListenerMouse::create();
+	this->touch_listener = cocos2d::EventListenerTouchOneByOne::create();
+}
+
+void DialogScreen::setupListeners() {
+	this->mouse_listener->onMouseDown = CC_CALLBACK_1(DialogScreen::onMouseDown, this);
+	this->mouse_listener->onMouseUp = CC_CALLBACK_1(DialogScreen::onMouseUp, this);
+	this->mouse_listener->onMouseMove = CC_CALLBACK_1(DialogScreen::onMouseMove, this);
+
+	this->touch_listener->onTouchBegan = CC_CALLBACK_2(DialogScreen::onTouchBegan, this);
+	this->touch_listener->onTouchMoved = CC_CALLBACK_2(DialogScreen::onTouchMoved, this);
+	this->touch_listener->onTouchEnded = CC_CALLBACK_2(DialogScreen::onTouchEnded, this);
+}
+
+void DialogScreen::onMouseDown(const cocos2d::Event* event) {
+
+}
+
+void DialogScreen::onMouseUp(const cocos2d::Event* event) {
+
+}
+
+void DialogScreen::onMouseMove(const cocos2d::Event* event) {
+
+}
+
+void DialogScreen::onTouchBegan(const cocos2d::Touch* touch, const cocos2d::Event* event) {
+
+}
+
+void DialogScreen::onTouchMoved(const cocos2d::Touch* touch, const cocos2d::Event* event) {
+
+}
+
+void DialogScreen::onTouchEnded(const cocos2d::Touch* touch, const cocos2d::Event* event) {
 
 }
 
@@ -93,6 +162,10 @@ MenuScreen::MenuScreen() {
 }
 
 short MenuScreen::load() {
+
+}
+
+void MenuScreen::setupListeners() {
 
 }
 
--- proj.win32/Screen.h
+++ proj.win32/Screen.h
@@ -5,6 +5,7 @@
 #include "cocos2d.h"
 
 #include <vector>
+#include <memory>
 
 namespace gnh {
 	
@@ -17,7 +18,17 @@ namespace gnh {
 		cocos2d::Vec2 position;
 		cocos2d::Vec2 size;
 
+		cocos2d::EventListenerTouchOneByOne* touch_listener = nullptr;
+		cocos2d::EventListenerKeyboard* keyboard_listener = nullptr;
+		cocos2d::EventListenerMouse* mouse_listener = nullptr;
+
+		bool has_touch = true;
+		bool has_keyboard = false;
+		bool has_mouse = true;
+
 		virtual short load();
+
+		virtual void setupListeners();
 
 		virtual void render()=0;
 		virtual void update();
@@ -37,6 +48,8 @@ namespace gnh {
 
 		short load();
 
+		void setupListeners();
+
 		void render();
 		void update();
 		void input(cocos2d::Event* event, cocos2d::Touch* touch = nullptr);
@@ -55,12 +68,24 @@ namespace gnh {
 
 		short load();
 
+		void setupListeners();
+
 		void render();
 		void update();
 		void input(cocos2d::Event* event, cocos2d::Touch* touch = nullptr);
 
 		void focus();
 		void blur();
+
+		//Mouse Events
+		void onMouseDown(const cocos2d::Event* event);
+		void onMouseUp(const cocos2d::Event* event);
+		void onMouseMove(const cocos2d::Event* event);
+
+		//Touch Events
+		void onTouchBegan(const cocos2d::Touch* touch, const cocos2d::Event* event);
+		void onTouchMoved(const cocos2d::Touch* touch, const cocos2d::Event* event);
+		void onTouchEnded(const cocos2d::Touch* touch, const cocos2d::Event* event);
 
 		short unload();
 
@@ -70,28 +95,34 @@ namespace gnh {
 
 	class ScreenManager {
 	private:
-		typedef std::vector<Screen> ScreenList;
-		typedef ScreenList::iterator ScreenListIt;
+		using ScreenList = std::vector<std::unique_ptr<Screen>>;
 
 		ScreenManager::ScreenList screens;
+
+		void setupScreen(Screen* screen);
+		void unsetupScreen(const std::unique_ptr<Screen>& screen);
+
+		void loadScreen(Screen* screen);
 
 	public:
 		ScreenManager();
 
-		short stackScreen(const Screen* screen);
-		const Screen& popScreen();
+		short stackScreen(std::unique_ptr<Screen> screen);
+		std::unique_ptr<Screen> popScreen();
 
-		void focusScreen(Screen& screen);
-		void focusScreenAt(const short index);
+		void focusScreen(Screen* screen);
+		void focusScreenAt(short index);
 
-		void showScreen(Screen& screen);
-		void hideScreen(Screen& screen);
+		void showScreen(Screen* screen);
+		void hideScreen(Screen* screen);
 
 		void removeScreenAt(const short index);
-		void loadScreen(Screen& screen);
 
 		void handleEvent(cocos2d::Event* event, cocos2d::Touch* touch=nullptr);
 		bool propagateEvent(cocos2d::Event* event, cocos2d::Touch* touch=nullptr);
+
+		void update();
+		void render();
 
 		~ScreenManager();
 	};
